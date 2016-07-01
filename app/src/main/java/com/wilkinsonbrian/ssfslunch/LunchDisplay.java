27c2a9ffbee9c92acf1b1686de38474f1c36f4ae
Brian Wilkinson <wilkibrian@gmail.com>
2016-07-01 13:40:28 -0400

    first commit

---
+++ app/src/main/java/com/wilkinsonbrian/ssfslunch/LunchDisplay.java
@@ -0,0 +1,81 @@
+package com.wilkinsonbrian.ssfslunch;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LunchDisplay extends Activity {
+    private TextView textView;
+    private static final String DEBUG_TAG = "HttpExample";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch_display);
+        textView = (TextView) findViewById(R.id.url_result);
+        new GetLunchMenuFromServer().execute("https://grover.ssfs.org/menus/word/document.xml");
+
+    }
+
+    public class GetLunchMenuFromServer extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                return downloadUrl(params[0]);
+            } catch (IOException e) {
+
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+
+        }
+
+        private String downloadUrl(String myurl) throws  IOException {
+            InputStream is = null;
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response is: " + response);
+                is = conn.getInputStream();
+
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+    }
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+}
